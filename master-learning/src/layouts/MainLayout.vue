a2ce2a9180885ad02da977c960890ba6902702aa
02514860105 <victor.costa@tjce.jus.br>
2025-07-09 22:21:03 -0300

    feat: Criando app que gera frases motivacionais

---
+++ master-learning/src/layouts/MainLayout.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="flex justify-between items-center q-pa-md">
+      <q-icon name="keyboard_arrow_left" size="25px"></q-icon>
+      <q-toolbar-title class="text-center"> Master Learning </q-toolbar-title>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="leftDrawerOpen = !leftDrawerOpen"
+      />
+      <div>Quasar v{{ $q.version }}</div>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Master Learning First Project
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Início',
+    caption: 'Início da aplicação',
+    icon: 'home',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Cursos',
+    caption: 'Ir para o site de cursos',
+    icon: 'school',
+    link: 'https://github.com/quasarframework'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  components: {
+    EssentialLink
+  },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.q-header {
+  background-color: #f95179;
+}
+
+.q-q-page-container{
+  padding-top: 65px !important;
+
+}
+
+</style>
---
+++ master-learning/src/pages/Index.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-page class="flex column flex-center container justify-around">
+    <q-img 
+      src="https://cdn-icons-png.flaticon.com/512/8090/8090646.png"
+      spinner-color="white" 
+      style="max-width: 200px; max-height: 200px;"/>
+
+    <h5 class="title q-ma-sm text-center">{{phrases[index]}}</h5>
+    <q-btn
+      rounded 
+      class="button q-ma-sm"
+      label="Gerar Frase" 
+      stack 
+      @click="randomPosition"
+    />
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'DailyPhrase', 
+  data(){
+    return {
+      phrases: ['Levanta, sacode a poeira, dá a volta por cima.',
+                'Respire fundo e fique atento às surpresas deliciosas que a vida preparou para você!',
+                'Nem todos os dias são bons, mas há algo bom em cada dia.',
+                'Respeite sua mente e trate seu corpo com carinho.',
+                'Um passo de cada vez. Até meio passo serve. Siga firme, não há motivo para ter pressa.',
+                'Suba o primeiro degrau com fé. Não é necessário que você veja toda a escada, apenas dê o primeiro passo.',
+                'Para hoje, vou usar minha melhor roupa: a fé.'
+      ],
+      index: 0,
+    };
+  },
+  methods: {
+    randomPosition(){
+      let index = parseInt(Math.random() * 7 + 0);
+      this.index = index;
+    },
+  },
+  beforeMount(){
+    this.randomPosition();
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.container{
+  background-color: #f95179;
+}
+
+.title{
+  color: #fff
+}
+
+.button{
+  width: 90%;
+  color: #fff;
+  height: 50px;
+  background-color: #262525;
+}
+</style>
