1d5701351de19ec821ddb5b99ce5b36229af18d9
02514860105 <victor.costa@tjce.jus.br>
2025-07-11 18:39:18 -0300

    feat: Criando o bottom menu

---
+++ instadev/src/components/BottomBar/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="full-width row justify-around">
+    <q-img src="../../assets/homeicon.svg"></q-img>
+    <q-img src="../../assets/searchicon.svg"></q-img>
+    <q-img src="../../assets/addphotoicon.svg"></q-img>
+    <q-img src="../../assets/hearticon.svg"></q-img>
+    <q-avatar size="35px">
+      <img src="https://cdn.quasar.dev/img/avatar.png">
+    </q-avatar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BottomBar',
+};
+</script>
+
+<style lang="scss" scoped>
+.q-img {
+  width: 70px;
+  cursor: pointer;
+}
+</style>
--- instadev/src/pages/main/Index.vue
+++ instadev/src/pages/main/Index.vue
@@ -1,19 +1,31 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="full-width">
-      <TopBar class="q-mb-lg"/>
-      <q-separator class="full-width"/>
+    <div class="full-width absolute-top q-mt-lg">
+      <TopBar/>
+      <q-separator class="full-width q-mt-sm"/>
+    </div>
+    <div class="container-bottom full-width absolute-bottom q-pb-lg">
+      <q-separator class="full-width q-mb-sm"/>
+      <BottomBar/>
     </div>
   </q-page>
 </template>
 
 <script>
 import TopBar from 'src/components/TopBar/Index';
+import BottomBar from 'src/components/BottomBar/Index';
 
 export default {
   name: 'MainPage',
   components: {
     TopBar,
+    BottomBar,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.container-bottom {
+  background-color: #fafafa;
+}
+</style>
