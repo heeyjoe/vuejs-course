f3a49bd86acdeb10db8b7800abfea97506bb0ba5
02514860105 <victor.costa@tjce.jus.br>
2025-07-10 22:00:54 -0300

    feat: Criando parcialmente tela de login

--- instadev/src/pages/sign_in/Index.vue
+++ instadev/src/pages/sign_in/Index.vue
@@ -1,24 +1,38 @@
 <template>
-  <q-page class="flex flex-center column">
-    <q-img src="../../assets/instadevlogo.svg" height="49px"></q-img>
-    <q-input filled v-model="text" label="Filled"/>
-    <q-input filled v-model="text" label="Filled"/>
-    <a href="#">Forgot password?</a>
-    <q-btn color="primary" label="Sign In"/>
-    <div class="flex row">
-    <q-img src="../../assets/facebooklogo.svg"/>
-      <a href="#">Sign in with Facebook</a>
+  <q-page class="flex flex-center column justify-center q-px-md">
+    <q-icon name="fas fa-chevron-left"
+            class="absolute-left q-ma-lg"
+            size="23px"
+            color="grey"/>
+    <div class="full-width column items-center justify-center">
+      <q-img class="logo q-mb-lg"
+           src="../../assets/instadevlogo.svg"></q-img>
+      <q-input filled v-model="email" label="E-mail" class="full-width q-mb-md"/>
+      <q-input filled v-model="password" label="Password" type="password" class="full-width q-mb-md"/>
+      <div class="full-width items-end column">
+        <a class="link q-mt-md" href="#">Forgot password?</a>
+      </div>
+      <q-btn color="primary"
+             label="Log In"
+             class="sign-in-button full-width q-mt-lg"
+             :disable="true"/>
+      <div class="flex full-width items-center justify-center q-mt-xl">
+        <q-img class="facebook-icon" src="../../assets/facebooklogo.svg"/>
+        <a class="link q-ml-xs" href="#">Log in with Facebook</a>
+      </div>
+      <div class="full-width row items-center justify-center q-my-xl">
+        <q-separator class="separator" inset/>
+        OR
+        <q-separator class="separator" inset/>
+      </div>
+      <div class="full-width row items-center justify-center text-black-opacity">
+        Don't have an account? <a class="link q-ml-xs" href="#">Sign up</a>
+      </div>
     </div>
-    <div class="full-width">
-      <q-separator />
-      or
-      <q-separator />
+    <div class="full-width column items-center justify-center absolute-bottom">
+      <q-separator class="full-width"/>
+      <div class="q-my-lg text-black-opacity"><span> Instagram of Facebook </span></div>
     </div>
-    <div>
-      Don't have an account? <a href="#">Sign up</a>
-    </div>
-    <q-separator/>
-    Instagram of Facebook
   </q-page>
 </template>
 
@@ -27,8 +41,31 @@ export default {
   name: 'SignIn',
   data() {
     return {
-      text: '',
+      email: '',
+      password: '',
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.logo {
+  height: 49px;
+  width: 182px;
+}
+
+.sign-in-button {
+  height: 44px;
+  border-radius: 5px;
+  background-color: #3797ef;
+}
+
+.facebook-icon {
+  height: 17px;
+  width: 17px;
+}
+
+.separator {
+  width: 35%;
+}
+</style>
