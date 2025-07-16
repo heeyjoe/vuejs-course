f12fe4a442cc2642fa0d8ed2f161cdbfce482770
02514860105 <victor.costa@tjce.jus.br>
2025-07-16 07:56:40 -0300

    feat: Criando area de avatares

--- instadev/src/pages/main/Index.vue
+++ instadev/src/pages/main/Index.vue
@@ -1,9 +1,21 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="full-width absolute-top q-mt-lg">
+  <q-page class="flex">
+    <div class="container-top full-width absolute-top q-mt-lg">
       <TopBar/>
       <q-separator class="full-width q-mt-sm"/>
     </div>
+    <q-scroll-area horizontal :visible="false" class="full-width scroll-area">
+      <div class="row no-wrap">
+        <div class="full-width" v-for="item in 10" :key="item">
+          <div class="column items-center justify-center q-mr-md">
+            <q-avatar size="62px">
+              <img class="avatar" src="https://cdn.quasar.dev/img/avatar.png">
+            </q-avatar>
+            <span>Nome</span>
+          </div>
+        </div>
+      </div>
+    </q-scroll-area>
     <div class="container-bottom full-width absolute-bottom q-pb-lg">
       <q-separator class="full-width q-mb-sm"/>
       <BottomBar/>
@@ -27,5 +39,24 @@ export default {
 <style lang="scss" scoped>
 .container-bottom {
   background-color: #fafafa;
+}
+
+.scroll-area {
+  height: 98px;
+  margin-top: 70px
+}
+
+.q-avatar{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: linear-gradient(#FBAA47, #D91A46, #A60F93);
+  padding: 2px;
+
+  .avatar{
+    width: 57px;
+    height: 57px;
+    border: solid 2px #ffff
+  }
 }
 </style>
